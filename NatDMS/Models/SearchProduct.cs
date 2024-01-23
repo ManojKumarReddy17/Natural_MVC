ce568ad26c0bf10cfc8b6f9f03ff7945a45bfc86
lokeshdotnet <bpmdotnet@gmail.com>
2024-01-23 16:38:52 +0530

    search started

---
+++ NatDMS/Models/SearchProduct.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NatDMS.Models
+{
+	public class SearchProduct
+	{
+        public string Category { get; set; }
+        public string ProductName { get; set; }
+    }
+}
+
