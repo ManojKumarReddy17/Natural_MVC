4767042708c955582422e9d0c312d5879bbf4a3a
lokeshdotnet <bpmdotnet@gmail.com>
2024-02-08 23:37:21 +0530

    commented code in guest products removed

--- NatDMS/Controllers/GuestProductController.cs
+++ NatDMS/Controllers/GuestProductController.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using NatDMS.Models;
 using Natural.Core.IServices;
 using Natural.Core.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace NatDMS.Controllers
 {
@@ -18,18 +12,17 @@ namespace NatDMS.Controllers
         private readonly ICategoryService _CategoryService;
         private readonly IProductService _ProductService;
         private readonly IMapper _Mapper;
-        private readonly IConfiguration _Configuration;
 
-
-        public GuestProductController(ICategoryService CategoryService, IProductService ProductService, IMapper mapper, IConfiguration Configuration, IOptions<ApiDetails> apiDetails, HttpClient httpClient)
+        public GuestProductController(ICategoryService CategoryService, IProductService ProductService, IMapper mapper)
         {
             _CategoryService = CategoryService;
             _Mapper = mapper;
-            _Configuration = Configuration;
             _ProductService = ProductService;
 
         }
-        // GET: /<controller>/
+
+
+        // GET: /<controller>/ // this controller to display products with no search options//
         public async Task<ActionResult<List<EditProduct>>> Index8()
         {
             var getproduct = await _ProductService.GetAllProduct();
@@ -38,10 +31,7 @@ namespace NatDMS.Controllers
             return View(viewmodel);
         }
 
-       
-
-     
-
+        //To Display The Products in Login Page  
         public async Task<ActionResult<List<EditProduct>>> Index11()
         {
             var getproduct = await _ProductService.GetAllProduct();
@@ -56,8 +46,6 @@ namespace NatDMS.Controllers
 
             return View(viewmodel1);
         }
-
-      
 
 
         [HttpPost]
