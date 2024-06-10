2710e6c1641543346b53bd3e683836e2600cf801
samreeth04 <Samrithps@gmail.com>
2024-06-10 17:18:00 +0530

    addedValidations

--- NatDMS/Models/CategoryViewModel.cs
+++ NatDMS/Models/CategoryViewModel.cs
@@ -8,8 +8,11 @@ namespace NatDMS.Models
     public class CategoryViewModel
     {
         public string Id { get; set; }
-        [Required(ErrorMessage = "First Name is required.")]
+
+        [Required(ErrorMessage = "The Cateogery name  is required.")]
+        [StringLength(50, ErrorMessage = " No special characters will allowed .")]
         public string CategoryName { get; set; }
+
 
     }
 }
